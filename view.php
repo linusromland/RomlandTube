b8279be7ff6bb46315c2a7d9d0d18d59ecb8eb65
linusromland <48645689+linusromland@users.noreply.github.com>
2020-04-05 19:11:43 +0200

    started on the view page.
    Started on designing the view page. not done yet. Lot left to do but its a start

--- view.php
+++ view.php
@@ -7,10 +7,17 @@ require("config.php");
         <link rel="stylesheet" href="CSS/header.css">
         <link rel="stylesheet" href="CSS/footer.css">
         <link rel="stylesheet" href="CSS/style.css">
+        <link rel="stylesheet" href="CSS/view.css">
+
     </head>
     <body>
         <?php
         include("header.php");
+        ?>
+        <div id="recommend">
+            <p id="recommendations"><b>Recommendations</b></p>
+        </div>
+        <?php
         $id = $_SERVER['QUERY_STRING'];
 
         $printtitle = "SELECT title FROM videos WHERE id LIKE ".$id; 
@@ -33,17 +40,19 @@ require("config.php");
         $query2 = "UPDATE videos SET views = $views WHERE id = ".$id.";";
         mysqli_query($con,$query2);
 
-
-        echo "<video controls>";
+        echo "<div id=\"video\">";
+        echo "<video width=\"1040\" height=\"650\" controls>";
         echo "<source src=\"$location\" type=\"video/mp4\">";
         echo "</video>";
-        echo "<h1>$title</h1>";
-        echo "<p>Views: $views</p>";
-        echo "<p>Uploaded by <a href=\"./user.php?$user\">$user</a></p>";
+        echo "<p id=\"vidtitle\"><b>$title<b></p>";
+        echo "<p id=\"numviews\">Views: $views</p>";
+        echo "<p id=\"uploadedby\">Uploaded by: <a href=\"./user.php?$user\">$user</a></p>";
+        echo "</div>";
         ?>
         <?php
         include("footer.html");
         ?>
+
     </body>
 </html>
 
